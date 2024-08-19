6c0afe419cc18adc4f1fc233a7a289c4cbae2449
dante <jamesmidzi@gmail.com>
2024-08-19 16:06:01 +0200

    update todo to form, wire:model modifiers

--- resources/views/livewire/todos.blade.php
+++ resources/views/livewire/todos.blade.php
@@ -1,7 +1,14 @@
 <div>
     <div style="margin: 3rem; font-size: 1.2rem;" class="">
-        <input type="text" wire:model="todo">
-        <button type="button" wire:click="add">Add todo</button>
+        <form wire:submit="add">
+            {{-- wire:model doesn't send any network request --}}
+            {{-- modifiers will enable live updates --}}
+            {{-- wire:model.live --}}
+            {{-- wire:model.change - when tabbed away --}}
+            <input type="text" wire:model="todo">
+            <button>Add todo</button>
+        </form>
+        <p>Current todo: {{ $todo }}</p>
         <ul>
             @foreach ($todos as $todo)
                 <li>{{ $todo }}</li>
